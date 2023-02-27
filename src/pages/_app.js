ebec0210a2c3d3590580bfabad3e9485f7be81e2
Diego Cardenas <dcardenas@rccl.com>
2023-02-26 21:28:08 -0500

    add vercel analytics package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import MainLayout from '@/layouts/MainLayout'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <MainLayout>
       <Component {...pageProps} />
+      <Analytics />
     </MainLayout>
   )
 }
